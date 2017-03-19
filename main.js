e8d42de8057061bd7691f6a41a33e21e712d2cc5
k66chen <k66chen@gmail.com>
2017-03-19 05:05:34 -0400

    minor text fixes

---
+++ main.js
@@ -0,0 +1,44 @@
+//creates canvas and calls the various game states
+//
+//
+//
+
+window.onload = function (){
+    game = new Phaser.Game (500,800,Phaser.CANVAS,'gameContainer');
+
+    game.state.add ("Game", gameState); 
+
+    game.state.start("Game",true,false);
+}
+
+var gameState = function (game){
+
+    this.preload = function (){
+        game.load.image('sky','assets/sky.png');
+        game.load.image('sky','assets/block.png');
+    }
+
+    this.create =  function () {
+        game.physics.startSystem(Phaser.Physics.BOX2D);
+        game.stage.backgroundColor = '#2d2d2d';
+
+        game.add.sprite (0,0,'sky');
+
+        var block = game.add.sprite(150,150, 'block');
+
+        block.inputEnabled = true;
+
+        //set up mouse controls
+        block.events.onInputUp.add (function (){
+            game.add.text (5,5,"clicked");
+        })
+
+    }
+
+    this.update = function(){
+    }
+
+    this.render = function (){
+    }
+
+}
